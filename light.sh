7c24da16bc64248cb8e068824d5eee7075e14a64
pgilsena <pgilsena@tcd.ie>
2017-06-05 10:47:36 +0100

    fixed bug in hour comparison operators

--- light.sh
+++ light.sh
@@ -14,21 +14,21 @@ EVENING=250
 NIGHT=100
 
 # work hours = 06:00 - 16:00
-if [ $CURRENT_HOUR -gt 6 -a $CURRENT_HOUR -lt 16 ]
+if [ $CURRENT_HOUR -ge 6 -a $CURRENT_HOUR -lt 16 ]
 then
     echo $WORK >  $B_PATH
 
-# AFTERNOON hours = 16:00 - 19:00
-elif [ $CURRENT_HOUR -gt 15 -a $CURRENT_HOUR -lt 19 ]
+# afternoon hours = 16:00 - 19:00
+elif [ $CURRENT_HOUR -ge 16 -a $CURRENT_HOUR -lt 19 ]
 then
     echo $AFTERNOON >  $B_PATH
 
-# EVENING hours = 19:00 - 21:00
-elif [ $CURRENT_HOUR -gt 18 -a $CURRENT_HOUR -lt 21 ]
+# evening hours = 19:00 - 21:00
+elif [ $CURRENT_HOUR -ge 19 -a $CURRENT_HOUR -lt 21 ]
 then
     echo $EVENING >  $B_PATH
 
-# NIGHT hours = 21:00 - 06:00
+# night hours = 21:00 - 06:00
 else
     echo $NIGHT >  $B_PATH
 fi
